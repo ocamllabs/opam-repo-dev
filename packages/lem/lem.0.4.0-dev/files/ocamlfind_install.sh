c4891d2578b7deb3ed73ff04af4a22973e0236c5
David Sheets <sheets@alum.mit.edu>
2014-03-28 11:04:33 +0000

    lem: Remove ocamlfind directory directive before install

--- packages/lem/lem.0.4.0-dev/files/ocamlfind_install.sh
+++ packages/lem/lem.0.4.0-dev/files/ocamlfind_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -ex
 
-ocamlfind install lem ocaml-lib/META.lem \
+sed "s/^directory[[:space:]]*=.*$//" < ocaml-lib/META.lem > ocaml-lib/META
+ocamlfind install lem ocaml-lib/META \
   ocaml-lib/_build/extract.a \
   ocaml-lib/_build/extract.cma \
   ocaml-lib/_build/extract.cmxa \
